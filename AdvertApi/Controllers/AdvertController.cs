52f9fa2ae78eff6352aad49ee355b1e709e069f0
Shekhar Gupta <skrgta@hotmail.com>
2022-02-12 13:00:04 +0530

    initial commit

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,65 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [Route("adverts/v1")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            this.advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201, Type=typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPost]
+        [Route("Confirm")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            bool confirmed;
+            try
+            {
+                confirmed = await advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,53 @@
+﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using AutoMapper;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbModel = mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.Now;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using var client = new AmazonDynamoDBClient();
+            using var context = new DynamoDBContext(client);
+            await context.SaveAsync(dbModel);
+
+            return dbModel.Id;
+        }
+
+        public async Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            using var client = new AmazonDynamoDBClient();
+            using var context = new DynamoDBContext(client);
+            var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+
+            if (record == null)
+                throw new KeyNotFoundException($"A record with ID={model.Id} was not found");
+
+            if(model.Status != AdvertStatus.Active)
+            {
+                record.Status = AdvertStatus.Active;
+                await context.SaveAsync(record);
+            }
+            else
+            {
+                await context.DeleteAsync(record);
+            }
+
+            return true;
+        }
+    }
+}
